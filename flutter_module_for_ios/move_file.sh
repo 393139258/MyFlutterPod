6383ac6943f921981cd9f16a3331b9cb55acdcb8
393139258 <393139258@qq.com>
2021-08-31 14:54:06 +0800

    1.0.1

--- flutter_module_for_ios/move_file.sh
+++ flutter_module_for_ios/move_file.sh
@@ -32,17 +32,17 @@ else
 fi
 
 echo "编译flutter"
-flutter build ios --debug
+#flutter build ios --debug
 #release下放开下一行注释，注释掉上一行代码
-#flutter build ios --release --no-codesign
+flutter build ios --release --no-codesign
 echo "编译flutter完成"
 mkdir $out
-cp -r build/ios/Debug-iphoneos/*/*.framework $out
+cp -r build/ios/Release-iphoneos/*/*.framework $out
 #release下放开下一行注释，注释掉上一行代码
 #cp -r build/ios/Release-iphoneos/*/*.framework $out
 #cp -r .ios/Flutter/App.framework $out
 #cp -r .ios/Flutter/engine/Flutter.framework $out
-cp -r build/ios/Debug-iphoneos/*.framework $out
+cp -r build/ios/Release-iphoneos/*.framework $out
 
 echo "复制framework库到临时文件夹: $out"
 
