7b16b82d0c8e4e4abfb77d35f26d35a90c3e056c
393139258 <393139258@qq.com>
2021-08-31 19:08:16 +0800

    432e

--- flutter_module_for_ios/lib/main.dart
+++ flutter_module_for_ios/lib/main.dart
@@ -20,7 +20,27 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> lists = ["第一盒", "第二盒", "第三盒"];
 
+  //用户原生传递消息过来
   static const eventChannel = EventChannel('com.nativeToFlutter');
+
+  //flutter回传参数
+  static const MethodChannel _methodChannel =
+      const MethodChannel("com.flutterToNative");
+
+  void flutterGetBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final String result =
+          await _methodChannel.invokeMethod('getBatteryLevel', 'jason');
+      batteryLevel = result;
+    } catch (e) {
+      batteryLevel = 'Failed to get battery level.';
+    }
+
+    setState(() {
+      lists.add(batteryLevel);
+    });
+  }
 
   @override
   void initState() {
@@ -30,6 +50,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _getData(dynamic data) {
+    // UserInfoModel userM = UserInfoModel.fromJson(data);
+
     String OCString = data.toString();
     if (OCString.length != 0) {
       setState(() {
@@ -56,21 +78,34 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildListViewCell(int index, String title) {
-    return Container(
-      height: 60,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            child: Text(title),
-            margin: EdgeInsets.only(left: 15),
-          ),
-          Container(
-            child: Text("第${index}条数据"),
-            margin: EdgeInsets.only(right: 15),
-          ),
-        ],
+    return InkWell(
+      onTap: flutterGetBatteryLevel,
+      child: Container(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              child: Text(title),
+              margin: EdgeInsets.only(left: 15),
+            ),
+            Container(
+              child: Text("第${index}条数据"),
+              margin: EdgeInsets.only(right: 15),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class UserInfoModel {
+  String? name;
+  String? age;
+  UserInfoModel({this.name, this.age});
+  UserInfoModel.fromJson(Map<String, dynamic> json) {
+    this.name = json["name"];
+    this.age = json["age"];
   }
 }
